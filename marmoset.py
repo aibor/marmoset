a8e041dfa5f9752aa20b522cfa2ded7d4afd0ff0
Tobias Böhm <aibo@aibor.de>
2015-02-21 10:35:12 +0100

    added settings and support for different config templates

--- marmoset.py
+++ marmoset.py
@@ -1,7 +1,15 @@
 from flask import Flask, request, url_for
 from werkzeug.exceptions import NotFound
 from lib.flask_ext import *
+from os import listdir
 from lib.pxe_client_config import PXEClientConfig
+try:
+    import settings
+except: pass
+
+
+if 'settings' in globals() and 'CFG_DIR' in vars(settings):
+    PXEClientConfig.CFG_DIR = settings.CFG_DIR
 
 
 app = make_json_app(__name__)
@@ -14,12 +22,27 @@ def create_rescue_entry():
 
     ip_address = request.form['ip_address']
 
+    if 'template' in request.form:
+        template = request.form['template']
+    else:
+        template = 'rescue'
+
     re = PXEClientConfig(ip_address)
 
     if re.exists():
         return json_response({}, 409)
 
-    re.create('rescue')
+    try:
+        re.create(template)
+    except FileNotFoundError:
+        return json_response(
+                {'message': 'Template not found: ' +
+                    template +
+                    '. Available templates: ' +
+                    ', '.join(listdir(PXEClientConfig.TMPL_DIR)) },
+                400
+                )
+
 
     location = url_for('rescue_entry', ip_address=ip_address)
     return json_response(vars(re), 201, {'Location': location})
