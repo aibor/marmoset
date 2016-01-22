011a334253f790389d5dba08fd17e5c86a864b7a
Sebastian Rakel <sebastian@devunit.eu>
2016-01-22 08:44:41 +0100

    Added InstallimageConfigCommand to get a plain text config

--- marmoset/webserver/installimage.py
+++ marmoset/webserver/installimage.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, make_response
 from flask.ext.restful import reqparse, Resource, url_for
 from werkzeug.exceptions import NotFound
 from .. import installimage
@@ -35,3 +35,13 @@ class InstallimageObject(Resource):
 
         location = url_for('installimageobject', _method='GET', mac=installimage_config.mac)
         return vars(installimage_config), 201, {'Location': location}
+
+
+class InstallimageConfigCommand(Resource):
+    def get(self, mac):
+        installimage_config = InstallimageConfig(mac)
+
+        response = make_response(installimage_config.get_content())
+        response.headers['content-type'] = 'text/plain'
+
+        return response
