8a0b2d42c751169282ed196eac98e5450c0d5d81
Sebastian Rakel <sebastian@devunit.eu>
2016-01-22 08:58:54 +0100

    Added delete method to api endpoint v1/installimage/<mac>
    We are now able to delete created config files, with the HTTP Delete
    method
    
    This fixes the other part of issue #16

--- marmoset/webserver/installimage.py
+++ marmoset/webserver/installimage.py
@@ -36,6 +36,15 @@ class InstallimageObject(Resource):
         location = url_for('installimageobject', _method='GET', mac=installimage_config.mac)
         return vars(installimage_config), 201, {'Location': location}
 
+    def delete(self, mac):
+        installimage_config = InstallimageConfig(mac)
+
+        if installimage_config.exists():
+            installimage_config.remove()
+            return '', 204
+        else:
+            abort(404)
+
 
 class InstallimageConfigCommand(Resource):
     def get(self, mac):
